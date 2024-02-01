2710556982b41164243d4a427acad4b565d3fab9
dihegde <34866764+dihegde@users.noreply.github.com>
2024-02-01 09:57:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
+  
+
 streamlit.title('My Parents New healthy Diner')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.text('🐔 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Eggs')
+streamlit.text('🥣 🥗 Avocado Toast')
